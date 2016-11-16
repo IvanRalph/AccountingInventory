36466ed74e495266e9a032c4cb7ebb0527267397
Jeremy <jbt1996torres@gmail.com>
2016-11-16 11:20:10 +0800

    Test

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inventory System - Login</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/sweetalert.css">
+	
+	<style>
+		form{
+			width:300px;
+			border: 1px solid black;
+			border-radius:5px;
+			box-shadow: 5px 5px 5px #888888;
+			padding: 10px;
+		}
+
+		.center{
+			margin:0 auto;
+			margin-top:10%;
+		}
+
+		.button{
+			text-align:center;
+		}
+
+	</style>
+</head>
+<body>
+	<div class="container">
+		<form method="" class="center">
+			<div class="form-group">
+				<label>Username:</label>
+				<input type="text" class="form-control">
+			</div>
+
+			<div class="form-group">
+				<label>Password:</label>
+				<input type="password" class="form-control">
+			</div>
+
+			<div class="form-group button">
+			</div>
+		</form>
+	</div>
+
+
+
+	<!--jQuery-->
+	<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+	<!--bootstrap-->
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<!--sweetalert-->
+	<script type="text/javascript" src="js/sweetalert.min.js"></script>
+	<!--custom-->P
+	<script type="text/javascript" src="js">
+</body>
+</html>
