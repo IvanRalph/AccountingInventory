8810d1aa8fe828f560818a00998f27088f45d126
Jeremy <jbt1996torres@gmail.com>
2016-11-24 09:42:23 +0800

    database

--- db_accounting.sql
+++ db_accounting.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 19, 2016 at 08:00 AM
--- Server version: 10.1.9-MariaDB
--- PHP Version: 5.6.15
+-- Generation Time: Nov 24, 2016 at 02:39 AM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,12 +27,46 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `items` (
+  `date_added` datetime NOT NULL,
   `item_id` int(10) NOT NULL,
   `item_name` varchar(20) NOT NULL,
   `supplier_name` varchar(20) NOT NULL,
   `price` int(6) NOT NULL,
-  `stock` int(10) NOT NULL
+  `stock` int(10) NOT NULL,
+  `total` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`date_added`, `item_id`, `item_name`, `supplier_name`, `price`, `stock`, `total`) VALUES
+('2016-11-23 07:03:24', 32, 'Tset', 'Se', 1, -4, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `date_added` date NOT NULL,
+  `sales_id` int(11) NOT NULL,
+  `item_id` int(11) DEFAULT NULL,
+  `item_name` varchar(30) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `total` decimal(10,2) NOT NULL,
+  `branch` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`date_added`, `sales_id`, `item_id`, `item_name`, `qty`, `price`, `total`, `branch`) VALUES
+('2016-11-23', 5, NULL, 'Tset', 4, 1, '0.00', ''),
+('2016-11-23', 6, NULL, 'Tset', 2, 1, '0.00', '');
 
 -- --------------------------------------------------------
 
@@ -66,6 +100,12 @@ ALTER TABLE `items`
   ADD PRIMARY KEY (`item_id`);
 
 --
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`sales_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -79,7 +119,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `item_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `item_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `sales_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `user`
 --
